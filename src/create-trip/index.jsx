7879ac6ab07e7d4375d6389c96795fe7c3947f7c
Bikila Keneni <bikilakeneni32@gmail.com>
2026-01-09 23:42:40 +0300

    Fix typo in travel preferences heading

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -119,7 +119,7 @@ function CreateTrip() {
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
       <h2 className="font-bold text-3xl">
-        Tell us your travel preferences 🏕️🌴
+        what is your travel preferences 🏕️🌴
       </h2>
       <p className="mt-3 text-gray-500 text-xl">
         provide some basic information, and our trip planner will generate
